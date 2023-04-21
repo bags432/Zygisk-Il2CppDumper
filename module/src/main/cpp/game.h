4f53fb6fcd12e7667e431d6424bc884c281f7c47
bags432 <65481088+bags432@users.noreply.github.com>
2023-04-21 17:32:20 +0800

    Create game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.ahlh.swyyz.aligames"
+#define GamePackageName "endless.nightmare.spectre.curse.horror.scary.free.android.cn"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
